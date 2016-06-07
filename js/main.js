b00cffd6ff988f51a8ce1f58a5141fa1814c4b1e
Manoj Pandey <manojpandey1996@gmail.com>
2016-06-07 18:33:58 +0530

    initial design

--- js/main.js
+++ js/main.js
@@ -2,8 +2,15 @@
 
 $(function() {
     $(".element0").typed({
-        strings: ["Bake", "Build", "Forge", "are Code Forge !"],
+        strings: ["Bake", "Build", "forge code !"],
         typeSpeed: 0,
-        loop: false
+        loop: false,
+        typeSpeed: 10,
+        // time before typing starts
+        startDelay: 0,
+        // backspacing speed
+        backSpeed: 0,
+        // time before backspacing
+        backDelay: 500
     });
 });
